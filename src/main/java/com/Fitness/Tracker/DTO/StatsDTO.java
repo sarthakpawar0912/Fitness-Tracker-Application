3e9b63d43f2f3225f068fa7add04b94ac9529408
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-02 23:02:21 +0530

    Updated features and fixed issues

--- src/main/java/com/Fitness/Tracker/DTO/StatsDTO.java
+++ src/main/java/com/Fitness/Tracker/DTO/StatsDTO.java
@@ -3,16 +3,14 @@ package com.Fitness.Tracker.DTO;
 
 import lombok.Data;
 
-
 @Data
 public class StatsDTO {
+
     private long achievedGoals;
     private long notAchievedGoals;
-
     private int steps;
-
     private Double distance;
-
     private int  totalCaloriesBurned;
     private  int duration;
+
 }
