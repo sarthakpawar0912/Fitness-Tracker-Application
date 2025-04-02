87cf7dd82cba3782278c3a76d12ccc5f7f1720ca
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-02 23:02:37 +0530

    Updated features and fixed issues

--- src/main/java/com/Fitness/Tracker/DTO/WorkoutDTO.java
+++ src/main/java/com/Fitness/Tracker/DTO/WorkoutDTO.java
@@ -6,13 +6,11 @@ import java.util.Date;
 
 @Data
 public class WorkoutDTO {
+
     private Long id;
-
     private String type;
-
     private Date date;
-
     private int duration;
-
     private int caloriesBurned;
+
 }
