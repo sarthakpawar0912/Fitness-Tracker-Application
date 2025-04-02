6409405a5fbf376ac2b1ca0f1e2a693aa699ed3b
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-02 23:05:13 +0530

    Updated features and fixed issues

--- src/main/java/com/Fitness/Tracker/SERVICES/GOAL/GoalService.java
+++ src/main/java/com/Fitness/Tracker/SERVICES/GOAL/GoalService.java
@@ -1,11 +1,12 @@
 package com.Fitness.Tracker.SERVICES.GOAL;
 
 import com.Fitness.Tracker.DTO.GoalDTO;
-
 import java.util.List;
 
 public interface GoalService {
+
     GoalDTO postGoal(GoalDTO dto);
     List<GoalDTO> getGoals();
     GoalDTO updateStatus(Long id);
+
 }
