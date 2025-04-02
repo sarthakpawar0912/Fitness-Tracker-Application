aa3579fc544037977f0524c54fba5ce59fe54ba9
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-02 23:01:57 +0530

    Updated features and fixed issues

--- src/main/java/com/Fitness/Tracker/DTO/GoalDTO.java
+++ src/main/java/com/Fitness/Tracker/DTO/GoalDTO.java
@@ -6,9 +6,11 @@ import java.util.Date;
 
 @Data
 public class GoalDTO {
+
     private Long id;
     private String description;
     private Date startDate;  // Changed startdate → startDate
     private Date endDate;    // Changed enddate → endDate
     private boolean achieved;
+
 }
