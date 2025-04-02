0c29c51d9876ac81a14ad75d47eae85a128e8656
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-02 23:11:57 +0530

    Updated features and fixed issues

--- src/main/java/com/Fitness/Tracker/SERVICES/WORKOUT/WorkoutService.java
+++ src/main/java/com/Fitness/Tracker/SERVICES/WORKOUT/WorkoutService.java
@@ -5,6 +5,8 @@ import com.Fitness.Tracker.DTO.WorkoutDTO;
 import java.util.List;
 
 public interface WorkoutService {
+
     WorkoutDTO postWorkout(WorkoutDTO workoutDTO);
     List<WorkoutDTO> getWorkouts();
+
 }
